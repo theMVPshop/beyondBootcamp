afef280ea08f080e0585f084bec30006a23477bd
ArielHolman <ArielARodrigues01@gmail.com>
2021-03-23 10:16:07 -0500

    created router so I can start on API connection on the dashboard form. Dashboard has notes for me to get started on.

---
+++ src/Router.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router";
+import cookie from "cookie";
+
+//Components
+import About from "./components/About/About";
+import Dashboard from "./components/Dashboard/Dashboard";
+//Once Landing page is complete, we can uncomment the next line
+// import LandingPage from "./components/LandingPage/LandingPage";
+import SignIn from "./components/SignIn/SignIn";
+import YouTube from "./components/YouTube/YouTube";
+//Once Landing page is complete, we can delete article card from being imported
+import ArticleCard from './components/LandingPage/ArticleCard';
+
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie);
+  return cookies["loggedIn"] ? true : false;
+};
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        checkAuth() ? <Component {...props} /> : <Redirect to="/signn" />
+      }
+    />
+  );
+};
+
+const Router = () => {
+  return (
+    <Switch>
+      {/* Once Landing page is complete, we need to replace ArticleCard with LandingPage */}
+      <Route exact path="/" component={ArticleCard} />
+      <Route path="/about" component={About} />
+      <Route path="/youtube" component={YouTube} />
+      {/* <ProtectedRoute path="/dashboard" component={Dashboard} /> */}
+      <Route path="/dashboard" component={Dashboard} />
+      {/* <ProtectedRoute path="/signin" component={SignIn} /> */}
+      <Route path="/signin" component={SignIn} />
+    </Switch>
+  );
+};
+
+export default Router;
