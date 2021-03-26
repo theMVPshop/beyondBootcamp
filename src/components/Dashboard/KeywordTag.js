929430eaa6d00e831a263eac4eb07f41f6089668
ArielHolman <ArielARodrigues01@gmail.com>
2021-03-26 15:39:29 -0500

    Removed unneeded lines of code from index.html. Crompleted the design for the dashboard form. Need to add fued lines of code from html. Completed front end design for dashboard. Need to add functionality next.

---
+++ src/components/Dashboard/KeywordTag.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./dashboard.css";
+
+const Keyword = (props) => {
+  const [tags, setTags] = React.useState([]);
+  const addTags = (event) => {
+    if (event.key === "Enter" && event.target.value !== "") {
+      setTags([...tags, event.target.value]);
+      props.selectedTags([...tags, event.target.value]);
+      event.target.value = "";
+    }
+  };
+  const removeTags = (index) => {
+    setTags([...tags.filter((tag) => tags.indexOf(tag) !== index)]);
+  };
+
+  return (
+    <div className="tags-input">
+      <ul id="tags">
+        {tags.map((tag, index) => (
+          <li key={index} className="tag">
+            <span className="tag-title">{tag}</span>
+            <span className="tag-close-icon" onClick={() => removeTags(index)}>
+              x
+            </span>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onKeyUp={(event) => addTags(event)}
+        placeholder="Press enter to add tags"
+      />
+    </div>
+  );
+};
+
+export default Keyword;
