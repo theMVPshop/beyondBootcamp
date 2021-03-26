ad9df2ceea5f6d31e3b227e0039b6cbf9d6ac90e
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2021-03-26 11:03:04 -0500

    positioned navigation and reformatted grid size

--- src/components/LandingPage/ArticleCard.js
+++ src/components/LandingPage/ArticleCard.js
@@ -12,17 +12,18 @@ const ArticleCard = () => {
       {blogs.map((blog, id) => (
         <Card
           key={id}
-          style={{ height: "20vh", margin: "10px" }}
+          style={{ height: "25vh", margin: "10px" }}
           className="bg-dark text-white box1"
         >
           <Card.Img style={{ width: "18rem", borderRadius: "5px" }}>
             {/* {blog.image} */}
           </Card.Img>
           <Card.ImgOverlay className="card-overlay">
-            <Card.Title>{blog.title}</Card.Title>
-            <Card.Text style={{ height: "50px", overflow: "hidden" }}>
+            <Card.Title style={{fontSize:".9rem"}}>{blog.title}</Card.Title>
+            {/* description and date only visible on hover */}
+            {/* <Card.Text style={{ height: "20px", overflow: "hidden" }}>
               {blog.description}
-            </Card.Text>
+            </Card.Text> */}
             <Card.Text>{blog.date}</Card.Text>
           </Card.ImgOverlay>
         </Card>
--- src/components/LandingPage/NavCard.js
+++ src/components/LandingPage/NavCard.js
@@ -2,21 +2,35 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavCard = () => {
   return (
     <div className="box2">
       <Card style={{ height: "100%" }} className="bg-dark text-white">
         <Card.Body>
-          <Card.Title>Beyond Bootcamp</Card.Title>
-          <Card.Text>nada nada nada</Card.Text>
+          <Card.Title style={{ fontSize: "5rem", padding: "5% 5%"}}>Beyond Bootcamp</Card.Title>
+          <Card.Text></Card.Text>
           <div className="main-nav">
             <Navbar bg="dark" variant="dark">
-              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Navbar.Brand href="#home">BB</Navbar.Brand>
               <Nav className="mr-auto">
                 <Nav.Link href="#home">About</Nav.Link>
                 <Nav.Link href="#features">Tutorials</Nav.Link>
                 <Nav.Link href="#pricing">Social Links</Nav.Link>
+                <NavDropdown title="Search" id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
               </Nav>
             </Navbar>
           </div>
