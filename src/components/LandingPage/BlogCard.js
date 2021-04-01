5a044980c85dab4ca63ccd1d51e9b0cbabf5056c
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2021-04-01 01:02:21 -0500

    the grid is beautiful and the nav is sticky

--- src/components/LandingPage/BlogCard.js
+++ src/components/LandingPage/BlogCard.js
@@ -4,28 +4,23 @@ import Card from "react-bootstrap/Card";
 import "./landingPage.css";
 
 const BlogCard = (props) => {
-//   console.log("BLOGS", blogs);
-  return ( 
-      <>
-            <Card
-              key={props.id}
-              style={{ height: "25vh", margin: "10px" }}
-              className="bg-dark text-white box1"
-            >
-              <Card.Img style={{ width: "18rem", borderRadius: "5px" }}>
-                {/* {blog.image} */}
-              </Card.Img>
-              <Card.ImgOverlay className="card-overlay">
-                <Card.Title style={{fontSize:".9rem"}}>{props.blog.title}</Card.Title>
-                {/* description and date only visible on hover */}
-                {/* <Card.Text style={{ height: "20px", overflow: "hidden" }}>
-                  {blog.description}
+  //   console.log("BLOGS", blogs);
+  return (
+    <>
+      <Card key={props.id} className="bg-dark text-white box1">
+        <Card.Img>{/* {props.blog.image} */}</Card.Img>
+        <Card.ImgOverlay className="card-overlay">
+          <Card.Title style={{ fontSize: ".9rem" }}>
+            {props.blog.title}
+          </Card.Title>
+          {/* <Card.Text className="blog-description" style={{ fontSize:".6rem", overflow: "hidden" }}>
+                  {props.blog.description}
                 </Card.Text> */}
-                <Card.Text>{props.blog.date}</Card.Text>
-              </Card.ImgOverlay>
-            </Card>
-        </>
-      );
-    };
+          <Card.Text style={{ fontSize: ".8rem" }}>{props.blog.date}</Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+    </>
+  );
+};
 
 export default BlogCard;
--- src/components/LandingPage/NavBar.js
+++ src/components/LandingPage/NavBar.js
@@ -5,24 +5,26 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
   return (
-          <div className="main-nav sticky-top">
-            <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Beyond Bootcamp</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">About</Nav.Link>
-      <Nav.Link href="#features">Tutorials</Nav.Link>
-      <Nav.Link href="#pricing">Socials</Nav.Link>
-    </Nav>
-    <NavDropdown title="Filter" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Tools</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Front End</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Back End</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.4">Tech News</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.5">Future of Code</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.6">Personal Development</NavDropdown.Item>
-      </NavDropdown>
-  </Navbar>
-          </div>
+    <div className="main-nav sticky-top">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Beyond Bootcamp</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#features">Tutorials</Nav.Link>
+          <Nav.Link href="#pricing">Socials</Nav.Link>
+        </Nav>
+        <NavDropdown title="Filter" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Tools</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Front End</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Back End</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Tech News</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.5">Future of Code</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.6">
+            Personal Development
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Navbar>
+    </div>
   );
 };
 
