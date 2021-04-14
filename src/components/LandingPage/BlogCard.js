d806fb3d3dc1646433067e02ee235d791db6052d
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2021-04-14 10:42:31 -0500

    blog card edits

--- src/components/LandingPage/BlogCard.js
+++ src/components/LandingPage/BlogCard.js
@@ -8,10 +8,9 @@ const BlogCard = (props) => {
   const blogImage = props.blog.image ?? "https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80";
   return (
     <>
-      {/* <a href={props.blog.url}> */}
         <Card className="bg-dark text-white box1 desc-btn-1">
-          {/* <Card.Img src={props.blog.image} alt="Card image"/>
-        <Card.ImgOverlay> */}
+           {/* <Card.Img src={props.blog.image} alt="Card image"/> */}
+        {/* <Card.ImgOverlay>  */}
           <Card.Text
             className="blog-card-category"
             style={{ fontSize: ".8rem" }}
@@ -20,15 +19,14 @@ const BlogCard = (props) => {
           </Card.Text>
           <Image id="blog-card-image" src={blogImage} fluid />
           <Card.Title className="blog-card-title">
-            {props.blog.title}
+            {props.blog.title} 
           </Card.Title>
-          {/* <Card.Text className="blog-description" style={{ fontSize:".8rem", overflow: "hidden" }}>
+          <Card.Text className="blog-description" style={{ fontSize:".8rem", overflow: "hidden" }}>
                   {props.blog.description}
-                </Card.Text> */}
+                </Card.Text>
           {/* <Card.Text><WhatshotIcon/></Card.Text> */}
           {/* </Card.ImgOverlay> */}
         </Card>
-      {/* </a> */}
     </>
   );
 };
