02e2234060ae4e5cb68435c2ded452db13767918
Michael Scoggins <rhythmghosts@gmail.com>
2021-06-24 18:44:19 -0500

    it's fine

--- src/server.js
+++ src/server.js
@@ -14,32 +14,37 @@ const youtubeRouter = require("../server/routes/youtube");
 const port = process.env.PORT || 4001;
 app.use(express.static("build"));
 // app.use(express.static(path.join("../", "build")));
+app.use(cors());
 app.use(bodyParser.json());
 
-let corsOptions = {
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions), function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Content-Type", "application/json");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "*");
-  next();
-});
+// let corsOptions = {
+//   optionsSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions), function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Content-Type", "application/json");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "*");
+//   next();
+// });
 
 app.use(
-  "/",
-  cors(corsOptions),
+  // "/",
+  // cors(corsOptions),
   usersRouter,
   blogsRouter,
   emailsRouter,
   peekalinkRouter,
   youtubeRouter
 );
+
+app.get("/", (req, res) => {
+  res.send("beyond bootcamp");
+});
 
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}!`);
