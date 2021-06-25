642b8ea3efd8bd92b96ca8d0c7adf08c11066ad1
Michael Scoggins <rhythmghosts@gmail.com>
2021-06-24 20:09:24 -0500

    unbreak attempt #2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    // <BrowserRouter>
+    <Router />
+    // </BrowserRouter>
   );
 }
 
