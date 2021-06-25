ea278a7a9f6d6e2889884521cb98af6b7d4ca8ca
Michael Scoggins <rhythmghosts@gmail.com>
2021-06-24 19:50:43 -0500

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Router from "./Router";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
